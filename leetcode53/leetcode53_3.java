9b5f4e0bbb22942d77238401148bec7f35507abc
cc531 <cindyhsu2021@u.northwestern.edu>
2021-07-07 21:41:56 -0500

    07.07

--- leetcode53/leetcode53_3.java
+++ leetcode53/leetcode53_3.java
@@ -1,25 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
         int max_ans = nums[0];
 
-        for (int i = 1; i < n; i++) {
-
-            // greedy
-            if (nums[i - 1] > 0)
+        // greedy
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > 0) {
                 nums[i] += nums[i - 1];
+
+            }
             max_ans = Math.max(max_ans, nums[i]);
         }
 
         return max_ans;
+
     }
 }
-// [1] = 1
-// [1, 2] = max([1] + 2, [2])
-// [1, 2, 3] = max([1, 2] + 3, [3])
-
-// Runtime: 0 ms, faster than 100.00% of Java online submissions for Maximum
-// Subarray.
-// Memory Usage: 38.7 MB, less than 72.29% of Java online submissions for
-// time complexity:O(n)
-// space complexity:O(1)
